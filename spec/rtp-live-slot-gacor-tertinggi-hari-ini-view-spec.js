d627d5761f6d5b8345a93b43bdcc6b59f0896c65
flyhigh21 <readyplayerone122@outlook.com>
2022-07-18 21:31:06 +0700

    rtp live slot

---
+++ spec/rtp-live-slot-gacor-tertinggi-hari-ini-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import RtpLiveSlotGacorTertinggiHariIniView from '../lib/rtp-live-slot-gacor-tertinggi-hari-ini-view';
+
+describe('RtpLiveSlotGacorTertinggiHariIniView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
